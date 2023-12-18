0b7bfda9d5e50a5dcd79ac518b46c5bf350a9381
Pradeep Venkatesh <Pradeep Venkatesh@TYSS-Pradeep-Venkatesh>
2023-12-18 11:22:59 +0530

    HiiiiiiPradeep

---
+++ seleniumFireFlink/src/demopradeep/DummyPradeep.java
@@ -0,0 +1,10 @@
+package demopradeep;
+
+public class DummyPradeep {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hiii..........");
+	}
+
+}
