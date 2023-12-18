c4628e509cc57e5673a21c136708cb0750cc02c4
Pradeep Venkatesh <Pradeep Venkatesh@TYSS-Pradeep-Venkatesh>
2023-12-18 11:30:15 +0530

    Github added

---
+++ seleniumFireFlink/src/gitHub/GitHubPractice.java
@@ -0,0 +1,10 @@
+package gitHub;
+
+public class GitHubPractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("GithubPractice class added");
+	}
+
+}
